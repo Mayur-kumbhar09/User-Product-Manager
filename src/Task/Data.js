104616f2dce03362349683c00e6c962eea31be23
Mayur Hanmant Kumbhar <84976636+Mayur-kumbhar09@users.noreply.github.com>
2026-09-10 14:11:40 +0530

    added the avatar icons
    updated the avatar icons instead of manual images

--- src/Task/Data.js
+++ src/Task/Data.js
@@ -1,105 +1,110 @@
+import Person from '@mui/icons-material/Person';
+import Person2 from '@mui/icons-material/Person2';
+import Person3 from '@mui/icons-material/Person3';
+import Person4 from '@mui/icons-material/Person4';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Face from '@mui/icons-material/Face';
 
-
-export const userList=[
+export const userList = [
     {
-        id:1,
-        img:require('../Images/index.jpg'),
-        name:"Mayur Kumbhar",
-        city:"Satara",
-        email:"kumbharmayur0002@gmail.com",
-        phone_no: 7522972886 ,
-        address:"A/P-: Bhuinj Tal-: Wai Dist-: Satara",
-        products:[
+        id: 1,
+        icon: Person,
+        name: "Mayur Kumbhar",
+        city: "Satara",
+        email: "kumbharmayur0002@gmail.com",
+        phone_no: 7522972886,
+        address: "A/P-: Bhuinj Tal-: Wai Dist-: Satara",
+        products: [
             {
-                id:1 ,
-                product_name:"Laptop",
-                brand:"MI",
-                price:"50,000"
+                id: 1,
+                product_name: "Laptop",
+                brand: "MI",
+                price: "50,000"
             }
         ]
     },
     {
-        id:2,
-        img:require('../Images/index2.jpg'),
-        name:"Mujjamill Shaikh",
-        city:"Satara",
-        email:"shaikhmujjamill123@gmail.com",
-        phone_no: 8668205361 ,
-        address:"A/P-: Godoli , Satara",
-        products:[
+        id: 2,
+        icon: Person2,
+        name: "Mujjamill Shaikh",
+        city: "Satara",
+        email: "shaikhmujjamill123@gmail.com",
+        phone_no: 8668205361,
+        address: "A/P-: Godoli , Satara",
+        products: [
             {
-                id:1 ,
-                product_name:"Laptop",
-                brand:"Asus",
-                price:"1,00,000"
+                id: 1,
+                product_name: "Laptop",
+                brand: "Asus",
+                price: "1,00,000"
             }
         ]
     },
     {
-        id:3,
-        img:require('../Images/index3.jpg'),
-        name:"Amey Bhosale",
-        city:"Rahimatpur",
-        email:"bhosaleamey4321@gmail.com",
-        phone_no:9356477948,
-        address:"A/P-: Bhuinj Tal-: Wai Dist-: Satara",
-        products:[
+        id: 3,
+        icon: Person3,
+        name: "Amey Bhosale",
+        city: "Rahimatpur",
+        email: "bhosaleamey4321@gmail.com",
+        phone_no: 9356477948,
+        address: "A/P-: Bhuinj Tal-: Wai Dist-: Satara",
+        products: [
             {
-                id:1 ,
-                product_name:"Laptop",
-                brand:"Asus",
-                price:"50,000"
+                id: 1,
+                product_name: "Laptop",
+                brand: "Asus",
+                price: "50,000"
             }
         ]
     },
     {
-        id:4,
-        img:require('../Images/index4.jpg'),
-        name:"Harshad Shinde",
-        city:"Sangli",
-        email:"harshadshinde8959@gmail.com",
-        phone_no: 7020115521 ,
-        address:"A/P-: Sangali ,  Sangli",
-        products:[
+        id: 4,
+        icon: Person4,
+        name: "Harshad Shinde",
+        city: "Sangli",
+        email: "harshadshinde8959@gmail.com",
+        phone_no: 7020115521,
+        address: "A/P-: Sangali ,  Sangli",
+        products: [
             {
-                id:1 ,
-                product_name:"Laptop",
-                brand:"HP",
-                price:"65,000"
+                id: 1,
+                product_name: "Laptop",
+                brand: "HP",
+                price: "65,000"
             }
         ]
     },
     {
-        id:5,
-        img:require('../Images/index5.jpg'),
-        name:"Tejas Pawar",
-        city:"Nashik",
-        email:"pawartejas9008@gmail.com",
-        phone_no: 7218500631 ,
-        address:"A/P-: Nashik Tal-: , Nashik",
-        products:[
+        id: 5,
+        icon: AccountCircleIcon,
+        name: "Tejas Pawar",
+        city: "Nashik",
+        email: "pawartejas9008@gmail.com",
+        phone_no: 7218500631,
+        address: "A/P-: Nashik Tal-: , Nashik",
+        products: [
             {
-                id:1 ,
-                product_name:"Laptop",
-                brand:"Dell",
-                price:"50,000"
+                id: 1,
+                product_name: "Laptop",
+                brand: "Dell",
+                price: "50,000"
             }
         ]
     },
     {
-        id:6,
-        img:require('../Images/index6.jpg'),
-        name:"Vaibhav Bidgar",
-        city:"Mahswad",
-        email:"bidgarvaibhav6790@gmail.com",
-        phone_no: 9356230052 ,
-        address:"A/P-: mahswad ,  Pandharpur",
-        products:[
+        id: 6,
+        icon: Face,
+        name: "Vaibhav Bidgar",
+        city: "Mahswad",
+        email: "bidgarvaibhav6790@gmail.com",
+        phone_no: 9356230052,
+        address: "A/P-: mahswad ,  Pandharpur",
+        products: [
             {
-                id:1 ,
-                product_name:"Laptop",
-                brand:"Aser",
-                price:"70,000"
+                id: 1,
+                product_name: "Laptop",
+                brand: "Aser",
+                price: "70,000"
             }
         ]
     }
